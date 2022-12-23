48df08f660e296c409968b1b092ce5f140b0a38c
ntua-el19707 <el19707@mail.ntua.gr>
2022-12-23 15:11:49 +0000

    authmiddleware

---
+++ api-backend/config/authmiddlewares/auth.js
@@ -0,0 +1,96 @@
+const {vaildPass} = require('../../utils/StringGen')
+const jsonwebtoken = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const pathToKey = path.join(__dirname, '../keys', 'private.pem');
+const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+const pathToPKey = path.join(__dirname, '../keys', 'public.pem');
+const Pub_KEY = fs.readFileSync(pathToPKey, 'utf8');
+exports.authuser= (req,res,next) =>{
+    //console.log(req.headers)
+    //let target_target_token = req.headers.authorization
+   // console.log(target_target_token)
+    try{
+    //  console.log(req.headers)
+    const token_parts = req.headers.authorization.split(' ')
+    //console.log(token_parts[0])
+     if( token_parts[0] === 'Bearer' && token_parts[1].match(/\S+\.\S+\.\S+/) !== null){
+      //patern match 
+      
+      try{
+      const  verificattion = jsonwebtoken.verify(token_parts[1],Pub_KEY,{algorithms:['RS256']})
+     // console.log(verificattion)}
+     req.jwt = verificattion;
+     next();}
+      catch(err){
+        //console.log(err)
+        res.status(401).json({success:false,msg:'You are anothorized!'})
+      }
+    }else{
+      res.status(401).json({success:false,msg:'You are anothoprized!'})
+    }
+}catch(err){
+    res.status(401).json({success:false,msg:'You are anothoprized!'})
+}
+  }
+exports.exist= (req,res,next)=>{
+    let mode  = req.body.mode ;
+    console.log(req.body);
+    if(mode){
+    if(mode === 'Change_pass'){
+       let email = req.body.login.user;
+       let pass = req.body.login.pass;
+       if(pass && email){
+       const user =  get_user(email);
+          if(user){
+            if(vaildPass(pass,user.saltpw,user.hashpw)){//validation
+                next();
+
+           }
+          else{
+             res.status(401).json({msg:"Wrong Pass Access Denied !",success:false})
+          }
+        }
+      }
+      else{
+        let msg = `Access Denied `;
+        if(!pass){
+          msg += `Password has not been given `
+        }
+        if(!email){
+          msg += `User has not been given `
+        }
+        res.status(401).json({msg:msg,success:false})
+       }
+
+    }else if(mode === 'Register_user'){
+      next();//non auth is required
+    }
+    else{
+      res.status(400).json({msg:'non right mode options',success:false})
+    }}else{
+      res.status(400).json({msg:'Mode is not provided',success:false})
+    }
+
+
+}
+function get_user(email){
+  let critentials ;
+  let fetchUser = new Promise ((resolve,reject)=>{
+   models.users.findOne({where:{email:email}}).then((user)=>{
+      critentials  = user.dataValues ;
+      resolve();
+    }).catch(err=>{
+      res.status(400).json({err:err,status:"error"})
+      resolve()
+    })})
+    Promise.all([fetchUser]).then(()=>{
+      return critentials;
+    })
+}
+exports.Criteria = (req,res,next) => {
+   //:To do implement if  poll requires Criteria to view 
+   next();
+} 
