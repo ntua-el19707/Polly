9bcabeff5c211c5e1e32662471cc55d2f979ba51
ntua-el19707 <el19707@mail.ntua.gr>
2023-02-10 13:56:10 +0000

    mypolls.js

--- api-backend/controllers/admins/mypolls.js
+++ api-backend/controllers/admins/mypolls.js
@@ -4,23 +4,39 @@ const { router } = require('../../app');
 const models = initModels(sequelize);
 
 exports.getmypolls = (req,res,next) =>{
-    const email  = req.body.Creator ; 
+    const email  = req.jwt.sub ; 
+    let resp ={} ; 
     if(email){
     let fetchmypolls  = new Promise((resolve,reject) =>{
         models.Polls.findAll({where:{
             email:email
         }}).then((row) =>{
-           res.status(200).json({Poll:row})
-            resolve();
+           // console.log(row)
+
+           /*QuestionarieTitle:,id:0 */
+            let polls = [] ;
+            row.forEach(r => {
+                polls = [...polls,{QuestionarieTitle:r.dataValues.title,id:r.dataValues.poll_id}]
+            });
+            //console.log(polls);
+            resp = {polls:polls};
+           //res.status(200).json({Poll:row})
+            resolve(resp);
 
         }).catch(err=>{
             console.log(err)
-            resolve();
+            resp ={err:err}
+            resolve(resp);
         })
        
+    
     })
-    Promise.all([fetchmypolls]).then(()=>{
-        res.send('hi')
+    Promise.all([fetchmypolls]).then((resp)=>{
+        if(resp.err){
+            res.status(400).json(resp)
+        }else{
+            res.status(200).json(resp)
+        }
     })
 }
     else{
