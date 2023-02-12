e5513e706794f1087572b70cfd35f02421923d07
ntua-el19701 <115795834+ntua-el19701@users.noreply.github.com>
2023-02-12 19:56:21 +0200

    Add files via upload

---
+++ resetall.js
@@ -0,0 +1,35 @@
+const sequelize = require('../../utils/database');
+var initModels = require("../../models/init-models");
+const { where } = require('sequelize');
+//const chalk = require('chalk');
+var models = initModels(sequelize);
+
+exports.DeleteMatehodresetall = (req,res,next) => {
+   
+    let deleteall = new Promise((resolve,reject)  => {
+        
+        
+        models.Polls.destroy({where:{}}).then ((d) => {
+        
+        models.users.destroy({where:{}}).then ((d) => {
+        
+        })
+            
+            
+            
+
+
+        }).catch(err =>{
+            console.log(err)
+            resolve();
+        })
+
+
+        })
+
+        
+
+
+res.status(200).json({status:"OK"});
+
+}
---
+++ resetq.js
@@ -0,0 +1,78 @@
+
+        // require models
+        const sequelize = require('../../utils/database');
+        var initModels = require("../../models/init-models");
+        const { where } = require('sequelize');
+        //const chalk = require('chalk');
+        var models = initModels(sequelize);
+        
+        
+       
+        exports.DeleteMatehodresetq = (req,res,next) =>{
+            let pid;
+            let reportsid = [];
+            let length;
+            const questionnaireID = req.params.resetq_id ;
+            if((!isNaN(questionnaireID))){
+                pid = parseInt(questionnaireID);
+
+            let findreport = new Promise((resolve,reject)  => {
+
+
+                models.report_answers.findAll({where:{poll_id:pid}}).then((r)=>{
+                     length = r.length;
+    
+                 for(let i = 0; i<length; i++){
+                    
+                    let report=r[i].dataValues;
+                    reportsid[i] = report.report_id;      
+                }
+                    resolve();
+                
+                }).catch(err =>{
+                    console.log(err)
+                    resolve();
+                })
+
+
+          })
+            
+            Promise.all([findreport]).then((ra)=>{
+                for(let i = 0; i < length; i ++){
+                  models.reportforstats.destroy({where:{report_id:reportsid[i]}}).then((r)=>{
+
+              })
+                    
+                   models.reportfornotstats.destroy({where:{report_id:reportsid[i]}}).then((r)=>{
+
+
+              }).catch(err =>{
+                console.log(err)
+                resolve();
+            })
+
+            }   
+
+            }).then(()=>{
+                models.report_answers.destroy({where:{poll_id:pid}});
+
+            }).catch(err =>{
+                console.log(err)
+                resolve();
+            })
+
+            }
+            else{  
+
+                res.status(400).json({status:"failed",reason:"Wrong Input"})
+            }
+
+            res.status(200).json({status:"okay"});
+
+
+        }
+  
+  
+     
+      
+        
