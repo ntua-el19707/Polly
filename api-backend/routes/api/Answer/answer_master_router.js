3cacf73002bb65e3f4696edc5bdb810eca4d1529
ntua-el19707 <el19707@mail.ntua.gr>
2023-02-14 21:27:29 +0000

    fixed routes for fromtened chane pass

--- api-backend/routes/api/Answer/answer_master_router.js
+++ api-backend/routes/api/Answer/answer_master_router.js
@@ -10,9 +10,13 @@ const doanswer  = require('./doanswer');
 const getsessionanswers  = require('./getsessionanswers');
 const getquestionanswers  = require('./getquestionanswers');
 const doanswerfill = require('./anwerstats')
+const {changepass} = require('../../../controllers/forgot')
+const {authuserForForgotpass} =require('../../../config/authmiddlewares/auth')
 router.use('/questionnaire',questionnaire);
 router.use('/question',question);
 router.use('/doanswer',doanswer)
 router.use('/fill',doanswerfill)
+router.use('/forgot/change',authuserForForgotpass,changepass)
+
 //router.use('/')
 module.exports =router 
--- api-backend/routes/user.js
+++ api-backend/routes/user.js
@@ -2,9 +2,7 @@ const {Router} = require('express');
 const router = Router() ;
 //import login - post controller
 const {login} = require('../controllers/login')
-router.route('/login').post(login).get((req,res,next)=>{
-    res.send('h');
-});
+router.route('/login').post(login)
 //to_do_later logout 
 //router.route('/logout').post(/*controller*/)
 module.exports = router ;
