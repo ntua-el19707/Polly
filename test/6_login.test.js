2eed03ba57550c1b9060c04f0f8f5e5a962cc48f
ntua-el18716 <116654394+ntua-el18716@users.noreply.github.com>
2023-02-14 20:16:04 +0200

    Add files via upload

---
+++ test/6_login.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const chai = require('chai')
+const expect = chai.expect;
+const app = require('../api-backend/app');
+require('custom-env').env('localhost'); 
+
+const owner = {
+    "user": "madara@root.com",
+    "pass": "12"
+}
+const validbody = {
+    Login:owner
+}
+const ownerWrongPass = {
+    "yser": "madara@root.com",
+    "password": "123"    
+}
+const invalidbody = {
+    Login:ownerWrongPass
+}
+
+let token;
+let response;
+describe('Test login route as Owner (Post Request: {baseurl}/login)', () => {
+    it('Should Login with status 200', (done) => {
+        request(app)
+        .post('/inteliq_api/login')
+        .send(validbody)
+        .end((err, res) => {
+            token = res.body.user.token;
+           // response = res.body;
+           console.log(res);
+            expect(res.status).to.eq(200);
+            done()
+        })
+    })
+    
+});
+
+/*
+describe('Test login route as Owner (Post Request: {baseurl}/login)', () => {
+    it('Should Login with status 401', (done) => {
+        request(app)
+        .post('/inteliq_api/login')
+        .send(invalidbody)
+        .end((err, res) => {
+           // token = res.body.token;
+            response = res.body;
+            expect(res.status).to.eq(401);
+            done()
+        })
+    })
+    
+});
+
+*/
