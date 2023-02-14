45c225230e207bd175f3486817e1e3faf41150e1
ntua-el19701 <115795834+ntua-el19701@users.noreply.github.com>
2023-02-14 20:17:50 +0200

    Add files via upload

---
+++ test/5_getquestionanswers.test.js
@@ -0,0 +1,57 @@
+
+const request = require('supertest');
+const chai = require('chai')
+const expect = chai.expect;
+const app = require('../api-backend/app');
+require('custom-env').env('localhost'); 
+
+const valid_quesstionnaire_id = 2;
+
+const invalid_quesstionnaire_id = 100;
+
+const valid_question_id = 4;
+
+const invalid_question_id = -5;
+
+
+let response;
+
+describe('Test getting question answers when questionnaire_id and session are valid (Post Request: {baseurl}/getquestionanswers/:questionnaireID/:questionID)', () => {
+    it('Should return totals with status 200', (done) => {
+        request(app)
+        .get('/inteliq_api/getquestionanswers/' + valid_quesstionnaire_id + "/" + valid_question_id )
+        .end((err, res) => {
+            response = res.body;
+            expect(res.status).to.eq(200);
+            done()
+        })
+        })
+});
+
+describe('Test getting question answers when questionnaire_id is invalid (Post Request: {baseurl}/getquestionanswers/:questionnaireID/:questionID)', () => {
+    it('Should return totals with status 400', (done) => {
+        request(app)
+        .get('/inteliq_api/getquestionanswers/' + invalid_quesstionnaire_id + "/" + valid_question_id )
+        .end((err, res) => {
+            response = res.body;
+            expect(res.status).to.eq(400);
+            done()
+        })
+        })
+});
+
+describe('Test getting question answers when question_id is valid (Post Request: {baseurl}/getquestionanswers/:questionnaireID/:questionID)', () => {
+    it('Should return totals with status 400', (done) => {
+        request(app)
+        .get('/inteliq_api/getquestionanswers/' + valid_quesstionnaire_id + "/" + invalid_question_id )
+        .end((err, res) => {
+            response = res.body;
+            expect(res.status).to.eq(400);
+            done()
+        })
+        })
+});
+
+
+
+
