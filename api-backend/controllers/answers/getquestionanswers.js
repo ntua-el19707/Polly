ed7a7a69c8b472623f22c24a0624929439986615
ntua-el19703 <116350448+ntua-el19703@users.noreply.github.com>
2023-02-14 16:02:07 +0200

    Delete getquestionanswers.js

--- api-backend/controllers/answers/getquestionanswers.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const router = Router() ;
-
-
-const {getanswers} =require('../../../controllers/answers/getquestionanswers')
-router.get('/:questionaireID/:questionID',getanswers);
-
-module.exports = router ;
