d427f50afa50a4fa3843020fc924a2237a05ecd3
ntua-el18716 <116654394+ntua-el18716@users.noreply.github.com>
2023-02-14 16:11:57 +0200

    Add files via upload

--- api-backend/controllers/answers/questionnaire2.js
+++ api-backend/controllers/answers/questionnaire2.js
@@ -4,23 +4,39 @@ var initModels = require("../../models/init-models");
 const e = require('express');
 const Questions = require('../../models/Questions');
 var models = initModels(sequelize);
-exports.questionnaire  = (req,res,next)=>{
+
+const JSONToCSV = (objArray, keys) => {
+    let csv = keys.join(',');
+    objArray.forEach((row) => {
+        let values = [];
+        keys.forEach((key) => {
+            console.log(key)
+            key.forEach(k=>{
+            values.push(row[k]|| '');})
+           // console.log(row);
+        });
+        csv += '\n' + values.join(',');
+    });
+    return csv;
+};
+
+
+exports.questionnaire2  = (req,res,next)=>{
 
 
     const questionaireID = req.params.questionnaireID ;
-    console.log(questionaireID);
+    const format = req.query.format;
 
     let pid;
+    
 
-    if(!isNaN(questionaireID)){
+    if(!isNaN(questionaireID) && (format === "csv" || format === "json" || format === undefined)){
 
         pid = parseInt(questionaireID);
 
         let rsp = {};
 
-  
         let found=false;
-
 
         let find_poll = new  Promise((resolve,reject)=>{
            
@@ -30,22 +46,15 @@ exports.questionnaire  = (req,res,next)=>{
 
                 if(length==0){
                     res.status(400).json({status:"failed",reason:`Poll with ID ${pid} does not exist`});
-  
                 }
                 else{
   
                    let poll=p[0].dataValues;
                    found=true;
-
                    rsp = {questionnaireID:`P${poll.poll_id}`,questionnaireTitle:poll.title,...rsp}
-
                 }
-
-               
-
-               
-                resolve();
-                
+    
+                resolve();                
                
             }).catch(err =>{
                 console.log(err)
@@ -76,10 +85,6 @@ exports.questionnaire  = (req,res,next)=>{
                     resolve();
 
                     })
-
-
-
-
                 })
 
                 Promise.all([find_keywords]).then(() =>{
@@ -117,7 +122,6 @@ exports.questionnaire  = (req,res,next)=>{
                             rsp.questions=json;
                             resolve();
 
-
                         })
 
                     })
@@ -125,28 +129,47 @@ exports.questionnaire  = (req,res,next)=>{
                     
                     Promise.all([find_questions]).then(() =>{
 
+                        
+
+                        if(format === 'csv'){
+
+                            var question_Arr = [];
+                            let keys = [];
+
+                            rsp.questions.forEach(option  => {
+                                row = {
+                                    questionaireID: rsp.questionnaireID,
+                                    questionnaireTitle: rsp.questionnaireTitle,
+                                    keywords: rsp.keywords,
+                                    qID: option.qID,
+                                    qtext: option.qtext,
+                                    required: option.required,
+                                    ...option
+                                }
+                                //console.log(row);
+                                question_Arr.push(row);
+                                //console.log(question_Arr);
+                            })
+                            
+                            keys.push(Object.keys(question_Arr[0]));
+                            //console.log(JSONToCSV(question_Arr,keys));
+                            rsp = (JSONToCSV(question_Arr,keys));
+
+                        }
+                        //console.log(rsp);
                         res.status(200).json({status:"okay",rsp});
+
                     })
 
 
                 })
 
-
-
-
-
-
-
             }
         })
-
-
-
 
     }
     else{
         res.status(400).json({status:"failed",reason:"You gave string instead of number"})
      }
    
-
 }
