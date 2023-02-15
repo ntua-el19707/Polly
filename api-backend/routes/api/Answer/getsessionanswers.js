d79764b6619f367c4e5a6a73f98941152c447158
ntua-el19703 <116350448+ntua-el19703@users.noreply.github.com>
2023-02-15 14:30:49 +0200

    Update getsessionanswers.js

--- api-backend/routes/api/Answer/getsessionanswers.js
+++ api-backend/routes/api/Answer/getsessionanswers.js
@@ -1,4 +1,7 @@
 const {Router} = require('express');
 const router = Router() ;
-router.get('/',/*Controller*/);
+
+const {getanswers} =require('../../../controllers/answers/getsessionanswers')
+router.get('/:questionaireID/:session',getanswers);
+
 module.exports = router ;
