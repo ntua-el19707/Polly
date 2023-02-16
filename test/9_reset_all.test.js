7fe2040e8777f7d01dc08811fab00d15cab3629b
ntua-el19707 <106684093+ntua-el19707@users.noreply.github.com>
2023-02-16 19:16:05 +0200

    Update 9_reset_all.test.js

--- test/9_reset_all.test.js
+++ test/9_reset_all.test.js
@@ -1,4 +1,3 @@
-/*
 const request = require('supertest');
 const chai = require('chai')
 const expect = chai.expect;
@@ -6,21 +5,16 @@ const app = require('../api-backend/app');
 //const { JsonWebTokenError } = require('jsonwebtoken');
 require('custom-env').env('localhost'); 
 
-const valid_quesstionnaire_id = 2;
-
-const invalid_quesstionnaire_id = 100;
-
-
 const owner = {
-    "user": "aa",
+    "user": "victor0601@live.com",
     "pass": "12"
 }
 const validbody = {
     Login:owner
 }
 const ownerWrongPass = {
-    "yser": "madara@root.com",
-    "password": "123"    
+    "yser": "victor0601@live.com",
+    "password": "12345abc"    
 }
 const invalidbody = {
     Login:ownerWrongPass
@@ -38,7 +32,7 @@ describe('Test reset all when user is not authorized (Delete Request: {baseurl}/
         .end((err, res) => {
            //let token = res.body.user.token;
           // let token="yJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWRhcmFAcm9vdC5jb20iLCJpYXQiOjE2NzYwNDIyMjU0NjUsImV4cCI6MTY3NjA0MjMxMTg2NX0.HcqNTMzber2C5ZBu7Z482svq6BcZQuHRJqOMHJJU1gNNtHL36Qh3B_T60Uw9mtRLRQ7PXNmQtAGt0do8U9P3NPSHbdv4HV7NMArOFtILRB-xZF1SLzKEEwq3lCoZMKkfM4c02iacSqXBQFUIiCD5wQ-a0Q-gi-Jv1PWkt1nnC0OJfcECXgpyE60yRCd94xgcDx_xEKu8V8WxvtQuI3AoONjGJQdAQrT-5i50WmAVAjg4Le-RGJgqqCmYMkflfshU441MVOC7fuxUGS7MUFaGWE9TQpndv-CQU_Wy3yihHhLIn3_X4aFCqyAdR2p6J0Et-O-nVI_yL7hwmVYy-raWv0EPUahT_cAKicO1xiDDNGGVpuCv-HtDHjjb7mfTjqbMxotBhnQhyqjLJ56Wt2lHk-E7ZOMOxv6prnk32m5Jjy0hqGz6v2WcQUUlOz1EV1JdJA2KsRI0GLP99DTVTkKhUNuFlJlDAKROsebEMd5vMJoSX7czaBd8qykpVLoOf9TKAFjdOWBEBlSfn7xKbVtxs7Pr4rlGo5HjM9jxa-3SrzXo2NrvwAKUQIvxtPZ8CK1lPXCvttKVeUCuhprW7yCdGUhM5J3lj1eBRKlSEcNFTdPIPHQbbB0b0RXcfdcn59jY1DCyu7lBqceBlzv6E126ZxWNBODXwWzZWfa1XsfSRoE"
-/*
+            /*
           let token=res.body.user.token;
           let token2=null;
          for(let i=7;i<token.length;i++){
@@ -48,7 +42,7 @@ describe('Test reset all when user is not authorized (Delete Request: {baseurl}/
           else
           token2=token2+token[i];
          }
-         
+         */
         let token="a";
 
            let url='/inteliq_api/admin/resetall';
@@ -105,8 +99,7 @@ describe('Test reset all when user is authorized (Delete Request: {baseurl}//int
         })
     })
     
-});
+})
 
 
 
-*/
